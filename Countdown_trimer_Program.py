41837e77ec9d191faf9060d2343e50b5b2075885
kumar viswajith <kumarviswajith303@gmail.com>
2025-03-22 22:41:41 +0530

    python programs

--- Countdown_trimer_Program.py
+++ Countdown_trimer_Program.py
@@ -11,3 +11,12 @@ for i in range(1,my_time):
     time.sleep(0.25)
 print("Times up!")
 
+for j in range(my_time, 0 ,-1):
+    # print(i)
+    seconds = j % 60
+    minutes = int(j/60)%60
+    hours = int(j /3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(0.25)
+print("Times up!")
+
