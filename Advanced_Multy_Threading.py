e9393c75dea9877cf620b66802c5a82f3922001e
kumar viswajith <kumarviswajith303@gmail.com>
2025-04-03 11:19:59 +0530

    Advanced Concepts

--- Advanced_Multy_Threading.py
+++ Advanced_Multy_Threading.py
@@ -8,11 +8,13 @@ def cubes(n):
     for i in n:
         time.sleep(1)
         print("cubes : ",i*i*i)
+
 def squares(n):
     print("squares : ")
     for i in n:
         time.sleep(1)
         print("squares : ",i*i)
+
 n = [1,2,3,4,5]
 t1 = threading.Thread(target=cubes,args=(n,))
 t2 = threading.Thread(target=squares,args=(n,))
