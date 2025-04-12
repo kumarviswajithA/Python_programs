7c6f4a2cc28822cad05c538144fc9ae29a131f84
kumar viswajith <kumarviswajith303@gmail.com>
2025-04-12 22:04:26 +0530

    Advanced Concepts

---
+++ Problem1_Longest_substring_without_repeating_character.py
@@ -0,0 +1,13 @@
+s = input("Enter your string : ")
+# s1 = list(s)
+# # print(s)
+# # print(s1)
+# s2 = []
+# for i in s1:
+#     if i not in s2:
+#         s2.append(i)
+# print(s2)
+# print(len(s2))
+s1 = set(s)
+print(s1)
+print(len(s1))
---
+++ Program2_nan_infinity.py
@@ -0,0 +1,10 @@
+import math
+
+n = math.nan
+print(n)
+
+x = float("inf")
+y = float("inf")
+
+print(x)
+print(y)
